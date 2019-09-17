84048b254717df1c12beba50055ce3aa7a03f9d3
takeo <yass@takibi.net>
2019-09-17 23:07:40 +0900

    Add loop query

--- umiget.py
+++ umiget.py
@@ -10,22 +10,21 @@ https://html.python-requests.org/
 
 
 class Umi:
-    LAYER_LIST='https://www.msil.go.jp/msilgisapi/api/layer/layer'
-
+    LAYER_LIST = 'https://www.msil.go.jp/msilgisapi/api/layer/layer'
 
     def __init__(self):
         self.session = HTMLSession()
         self.headers = {'Content-Type': 'application/json', 'Origin': 'https://www.msil.go.jp',
-           'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) Chrome/74.0.3729.131 Safari/537.36',
-            'X-Requested-With':'XMLHttpRequest'}
+                        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) Chrome/74.0.3729.131 Safari/537.36',
+                        'X-Requested-With':'XMLHttpRequest'}
         self.token = None
         self.base_url = 'https://www.msil.go.jp/arcgis/rest/services/Msil/'
 
     def login(self):
-        SESSION =  'https://www.msil.go.jp/msilgisapi/api/common/session'
+        SESSION = 'https://www.msil.go.jp/msilgisapi/api/common/session'
         r = self.session.post(SESSION, headers=self.headers)
 
-        TOKEN ='https://www.msil.go.jp/msilwebtoken/api/token/new';
+        TOKEN = 'https://www.msil.go.jp/msilwebtoken/api/token/new';
         r = self.session.post(TOKEN, headers=self.headers)
 
         self.token = r.json()['token']
@@ -37,8 +36,8 @@ class Umi:
         params['f'] = 'json'
 
         if query:
-            params['returnGeometry']='true'
-            params['spatialRel']='esriSpatialRelIntersects'
+            params['returnGeometry'] = 'true'
+            params['spatialRel'] = 'esriSpatialRelIntersects'
             params['geometryType'] = 'esriGeometryEnvelope'
             params['inSR'] = '4326'
             params['outSR'] = '4326'
@@ -87,7 +86,7 @@ class Umi:
         params = {}
         params['geometry'] = ",".join(map(str, extent))
         if all_data:
-            params['outFields']= ",".join(map(str, fields))
+            params['outFields'] = ",".join(map(str, fields))
 
         result = None
 
@@ -116,7 +115,7 @@ class Umi:
     def logout(self):
         self.session.close()
 
-    def get_obstacle(self, loop = True):
+    def get_obstacle(self, loop=True):
         '''
         海底障害物
         :param loop:
@@ -126,8 +125,7 @@ class Umi:
 
         return r
 
-
-    def get_light_house(self, loop = True):
+    def get_light_house(self, loop=True):
         '''
         灯台
         :param loop:
@@ -137,7 +135,7 @@ class Umi:
 
         return r
 
-    def get_float_lights(self, loop = True):
+    def get_float_lights(self, loop=True):
         '''
         灯浮標
         :param loop:
@@ -147,7 +145,7 @@ class Umi:
 
         return r
 
-    def get_pillar_lights(self, loop = True):
+    def get_pillar_lights(self, loop=True):
         '''
         灯標
         :param loop:
@@ -157,8 +155,7 @@ class Umi:
 
         return r
 
-
-    def get_other_lights(self, loop = True):
+    def get_other_lights(self, loop=True):
         '''
         灯(その他)
         :param loop:
@@ -168,8 +165,7 @@ class Umi:
 
         return r
 
-
-    def get_traffic_route_major(self, loop = True):
+    def get_traffic_route_major(self, loop=True):
         '''
         海交法航路
         :param loop:
@@ -179,8 +175,7 @@ class Umi:
 
         return r
 
-
-    def get_traffic_route_minor(self, loop = True):
+    def get_traffic_route_minor(self, loop=True):
         '''
         港測法航路
         :param loop:
@@ -190,8 +185,7 @@ class Umi:
 
         return r
 
-
-    def get_fisher(self, loop = True):
+    def get_fisher(self, loop=True):
         '''
         漁港
         :param loop:
@@ -201,7 +195,7 @@ class Umi:
 
         return r
 
-    def get_fisher_fix_net(self, loop = True):
+    def get_fisher_fix_net(self, loop=True):
         '''
         低地漁業権
         :param loop:
@@ -211,7 +205,7 @@ class Umi:
 
         return r
 
-    def get_marina(self, loop = True):
+    def get_marina(self, loop=True):
         '''
         マリーナ
         :param loop:
@@ -221,8 +215,7 @@ class Umi:
 
         return r
 
-
-    def get_swimming_beach(self, loop = True):
+    def get_swimming_beach(self, loop=True):
         '''
         海水浴場
         :param loop:
@@ -232,7 +225,7 @@ class Umi:
 
         return r
 
-    def get_tide_probe(self, loop = True):
+    def get_tide_probe(self, loop=True):
         '''
         潮汐観測所
         :param loop:
@@ -251,5 +244,41 @@ class Umi:
         r = self.query_data('Safety/MapServer/0', loop)
 
         return r
+
+    @staticmethod
+    def save_info(data_name):
+        umi = Umi()
+        umi.login()
+
+        r = eval('umi.get_' + data_name + '(True)')
+        with open('data/' + data_name + '.json', mode='w', encoding='utf-8') as f:
+            f.write(json.dumps(r, ensure_ascii=False))
+
+        umi.logout()
+
+
+if __name__ == '__main__':
+    Umi.save_info('obstacle')
+    Umi.save_info('light_house')
+    Umi.save_info('float_lights')
+    Umi.save_info('pillar_lights')
+    Umi.save_info('other_lights')
+    Umi.save_info('traffic_route_major')
+    Umi.save_info('traffic_route_minor')
+    Umi.save_info('fisher')
+    Umi.save_info('fisher_fix_net')
+    Umi.save_info('marina')
+    Umi.save_info('swimming_beach')
+#    Umi.save_info('tide_probe')
+#    Umi.save_info('safety_notice')
+
+
+
+
+
+
+
+
+
 
 
